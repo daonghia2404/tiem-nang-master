1d89c11c30b611600c49d818657adb054beec2e3
daonghia2404 <daonghia2404@gmail.com>
2022-08-10 22:23:02 +0700

    Change URL Image

--- src/components/BookBlock/BookBlock.tsx
+++ src/components/BookBlock/BookBlock.tsx
@@ -4,11 +4,11 @@ import classNames from 'classnames';
 import { TBookBlockProps } from './BookBlock.types.d';
 import './BookBlock.scss';
 
-const BookBlock: React.FC<TBookBlockProps> = ({ image, pathImage, name, isQuoteBook, onClick }) => {
+const BookBlock: React.FC<TBookBlockProps> = ({ images, image, pathImage, name, isQuoteBook, onClick }) => {
   return (
     <div className={classNames('BookBlock', { 'quote-book': isQuoteBook })} onClick={onClick}>
       <div className="BookBlock-image">
-        <img src={image || pathImage} alt="" />
+        <img src={images?.[0] || image || pathImage} alt="" />
       </div>
       {name && <div className="BookBlock-title">{name}</div>}
     </div>
--- src/components/BookBlock/BookBlock.types.d.ts
+++ src/components/BookBlock/BookBlock.types.d.ts
@@ -1,6 +1,7 @@
 export type TBookBlockProps = {
   name: string;
   image: string;
+  images: string[];
   pathImage?: string;
   isQuoteBook?: boolean;
   onClick?: () => void;
